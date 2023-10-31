3f48440d91ce9c86191ea22ce1683b8a2eddbe24
Nicolas Aguirre <inaguirre96@gmail.com>
2023-10-30 21:42:41 -0400

    basic colors generator working

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import openai
 from flask import Flask, render_template, request
 from dotenv import dotenv_values
+import json
+
 
 config = dotenv_values(".env")
 
@@ -8,12 +10,31 @@ app = Flask(__name__, template_folder="templates")
 openai.api_key = config["OPENAI_KEY"]
 
 
+def get_colors(msg):
+    prompt = f"""
+    Q: oncvert the following verbal description of a color palette into a list of colors: sage, nature, earth
+    A: ["#EDF1D6", "#609966", "#40513B"]
+
+    Desired Format: a JSON array of hexadecimal color codes
+
+    Q: Convert the following verbal description of a color palette into a list of colors: {msg}
+    A: 
+    """
+    response = openai.Completion.create(
+        prompt=prompt, model="gpt-3.5-turbo-instruct", max_tokens=200
+    )
+
+    colors = json.loads(response["choices"][0]["text"])
+    return colors
+
+
 @app.route("/palette", methods=["POST"])
 def prompt_to_palette():
     print("sarples")
-    app.logger.info(request.form.get("query"))
-    app.logger.info("GIT THE ROUTE")
-    # return "Reached route!"
+    query = app.logger.info(request.form.get("query"))
+    colors = get_colors(query)
+    return colors
+
 
 
 @app.route("/")
